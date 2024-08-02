248c3213c983b0aa930e2c90e1c54622c2e16f98
Dragnell27 <jaduvase27@gmail.com>
2024-08-02 11:28:14 -0500

    config bd

--- APIs/connection.php
+++ APIs/connection.php
@@ -1,10 +1,12 @@
 <?php
 function con()
 {
-    $db = 'if0_37024331_michat';
-    $host = 'sql112.infinityfree.com';
+    $db = 'railway';
+    $host = 'mysql.railway.internal';
     $puerto = 3306;
-    $user = 'if0_37024331';
-    $pass = 'kj6f3gelEgRwnd0';
+    $user = 'root';
+    $pass = 'DzrJHPLVIwkHgZymmAQkVFExvWxnfzxe';
     return mysqli_connect($host, $user, $pass, $db, $puerto);
 }
+
+$con = con();
