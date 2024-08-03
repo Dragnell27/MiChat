cb341427ea4c48a895117cf08b2f14371c1a17a3
Dragnell27 <jaduvase27@gmail.com>
2024-08-02 20:45:45 -0500

    Se corrige ruta de logout

--- src/js/index.js
+++ src/js/index.js
@@ -31,7 +31,7 @@ socket.on('DuplicateSession', (data) => {
 btn_logout.addEventListener('click', () => {
     socket.emit('logout',user);
     localStorage.removeItem('user');
-    window.location.href = 'http://localhost/MiChat/api/src/';
+    window.location.href = 'https://michat-production-a5c7.up.railway.app/d';
 });
 
 checkLocalStorageItem();
